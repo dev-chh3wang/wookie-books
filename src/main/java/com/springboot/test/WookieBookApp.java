d973e188e71fd6106b09a975c463b0ec460e1cea
Chhewang <chhewang.tmg@gmail.com>
2023-03-13 09:19:44 +0100

    initial setup

---
+++ src/main/java/com/springboot/test/WookieBookApp.java
@@ -0,0 +1,13 @@
+package com.springboot.test;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WookieBookApp {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WookieBookApp.class, args);
+	}
+
+}
